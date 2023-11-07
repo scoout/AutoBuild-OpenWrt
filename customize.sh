6154b3d802e4f718b93d67aa8a674be6a533cf32
iqbal fasya <79214502+scoout@users.noreply.github.com>
2023-11-07 21:47:51 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,7 +8,7 @@
 #1. Modify default IP
 # sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' openwrt/target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' openwrt/target/linux/x86/Makefile
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
