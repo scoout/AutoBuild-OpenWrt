fe13afd1ae78483754aa7ce49f05c8811834a60c
iqbal fasya <79214502+scoout@users.noreply.github.com>
2023-11-09 00:11:33 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,37 +11,7 @@
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' openwrt/target/linux/x86/Makefile
 
 #2. Clear the login password
- sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-echo '
-#!/bin/sh
-
-# lang
-uci set luci.main.lang=en
-uci commit luci
-
-# timezone
-uci set system.@system[0].timezone="WIB-7"
-uci set system.@system[0].zonename="Asia/Jakarta"
-uci commit
-
-# zram-swap
-uci set system.@system[0].zram_priority=100
-
-# ntp server
-uci -q delete system.ntp.server
-uci add_list system.ntp.server="0.id.pool.ntp.org"
-uci add_list system.ntp.server="1.id.pool.ntp.org"
-uci add_list system.ntp.server="2.id.pool.ntp.org"
-uci add_list system.ntp.server="3.id.pool.ntp.org"
-uci commit system && service sysntpd reload
-
-# uhttpd
-uci set uhttpd.main.rfc1918_filter=0
-uci set uhttpd.main.redirect_https=0
-uci commit uhttpd && service uhttpd reload
-
-exit 0
-' > openwrt/package/lean/default-settings/files/zzz-default-settings
+ # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
